558d1e6cf822d5c8f25bbb236223d96d6982a882
Rameshwor <r.sresta07@gmail.com>
2025-09-19 14:05:48 +0545

    feat(player): introduce player with basic rendering
    Add `PLAYER_RADIUS` to `constants.py` to define the player's size.
    Integrate the `Player` class into `main.py` and instantiate a player object.
    Implement basic player rendering on the screen.
    Refactor the game loop to use `pygame.time.Clock` for frame rate management and introduce a delta time (`dt`) for future physics calculations.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
+
 
 def main():
     print("Starting Asteroids!")
@@ -10,10 +12,24 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+
+        dt = clock.tick(60) / 1000
+        screen.fill((0, 0, 0))
+        player.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
 
 if __name__ == "__main__":
     main()
